c32f2373f3a40426c65117cd29a1a35371bef561
cristpm <crisdario21@gmail.com>
2016-09-26 21:46:59 +0200

    añadido los self y pasado pep8 en calcoohija.py

--- calcoohija.py
+++ calcoohija.py
@@ -4,15 +4,17 @@
 import sys
 import calcoo
 
+
 class CalculadoraHija(calcoo.Calculadora):
 
-	def multiply(op1,op2):
-		return op1*op2
+    def multiply(self, op1, op2):
+        return op1*op2
 
-	def div(op1,op2):
-		return op1/op2
+    def div(self, op1, op2):
+        return op1/op2
 
 if __name__ == "__main__":
+    calculadora = CalculadoraHija()
     try:
         operando1 = int(sys.argv[1])
         operando2 = int(sys.argv[3])
@@ -20,14 +22,14 @@ if __name__ == "__main__":
         sys.exit("Error: Non numerical parameters")
 
     if sys.argv[2] == "suma":
-        result = CalculadoraHija.plus(operando1, operando2)
+        result = calculadora.plus(operando1, operando2)
     elif sys.argv[2] == "resta":
-        result = CalculadoraHija.minus(operando1, operando2)
+        result = calculadora.minus(operando1, operando2)
     elif sys.argv[2] == "multiplica":
-        result = CalculadoraHija.multiply(operando1, operando2)
+        result = calculadora.multiply(operando1, operando2)
     elif sys.argv[2] == "divide":
         try:
-            result = CalculadoraHija.div(operando1, operando2)
+            result = calculadora.div(operando1, operando2)
         except ZeroDivisionError:
             sys.exit("Division by zero is not allowed")
 
