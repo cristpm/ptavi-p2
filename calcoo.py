3f85c98d83af3fd93c14a56cbd8e6f74230ed27d
cristpm <crisdario21@gmail.com>
2016-09-26 21:45:27 +0200

    añadido los self y pasado pep8 en calcoo.py

--- calcoo.py
+++ calcoo.py
@@ -5,24 +5,26 @@ import sys
 
 
 class Calculadora():
-	def plus (op1,op2):
-		return op1 + op2
 
-	def minus (op1,op2):
-		return op1 - op2
+    def plus(self, op1, op2):
+        return op1 + op2
+
+    def minus(self, op1, op2):
+        return op1 - op2
 
 if __name__ == "__main__":
-	try:
-		operando1 = int(sys.argv[1])
-		operando2 = int(sys.argv[3])
-	except ValueError:
-		sys.exit("Error: Non numerical parameters")
+    calculadora = Calculadora()
+    try:
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
 
-	if sys.argv[2] == "suma":
-		result = Calculadora.plus(operando1, operando2)
-	elif sys.argv[2] == "resta":
-		result = Calculadora.minus(operando1, operando2)
-	else:
-		sys.exit('Operación sólo puede ser sumar o restar.')
+    if sys.argv[2] == "suma":
+        result = calculadora.plus(operando1, operando2)
+    elif sys.argv[2] == "resta":
+        result = calculadora.minus(operando1, operando2)
+    else:
+        sys.exit('Operación sólo puede ser sumar o restar.')
 
-	print(result)
+    print(result)
